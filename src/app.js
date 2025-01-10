1f815c2dae4252ca355875e4f4ed6a9b44c71b21
Arvind <arvindri2005@gmail.com>
2025-01-11 01:40:34 +0530

    remove schedule task from main code

--- src/app.js
+++ src/app.js
@@ -15,10 +15,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Schedule the job to fetch crypto data every 2 hours
-schedule.scheduleJob("0 */2 * * *", fetchCryptoData);
-
-
+  
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
