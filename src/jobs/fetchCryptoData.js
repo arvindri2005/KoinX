596f19db7fa08bb9c50d743390094be944d5c5c4
Arvind <arvindri2005@gmail.com>
2025-01-11 02:19:34 +0530

    Checking github actions

--- src/jobs/fetchCryptoData.js
+++ src/jobs/fetchCryptoData.js
@@ -9,6 +9,7 @@ const fetchCryptoData = async () => {
 
     try {
         // Connect to MongoDB
+        console.log(process.env.MONGO_URI);
         mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
           
 
